6ea463300523ddb4b7a7a7c7c46278e4fee5c0c6
Теминатор <erofeev.aa@students.dvfu.ru>
2023-06-18 02:14:56 +1000

    Add burger-menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Context} from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import {NavLink} from "react-router-dom";
-import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts/RoutesConst";
-import {Button, Dropdown, Image} from "react-bootstrap";
+import {ADMIN_ROUTE, LOGIN_ROUTE, PRODUCT_ROUTE, SHOP_ROUTE} from "../utils/consts/RoutesConst";
+import {Button, Dropdown, Image, NavbarBrand} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
 import Container from "react-bootstrap/Container";
 // import {useNavigate} from 'react-router-dom'
@@ -16,11 +16,17 @@ import favourite from '../assets/favourite.svg'
 import userIcon from '../assets/user.svg'
 import basket from '../assets/basket.svg'
 import Basket from "./Basket";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
     // const navigate = useNavigate()
+    const [isOpen, setIsOpen] = useState(false);
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     const logOut = () => {
         user.delUser()
     }
@@ -60,20 +66,103 @@ const NavBar = observer(() => {
             </Navbar>
             <Navbar style={{height: '96px'}}>
                 <Container>
-                    <NavLink to={SHOP_ROUTE}>
-                        <Image src={logo}/>
-                    </NavLink>
-                    {/*<Dropdown className='mx-3'>
-                        <Dropdown.Toggle variant="dark" id="dropdown-basic">
-                            Каталог
-                        </Dropdown.Toggle>
+                    <div className="d-flex align-items-center">
+                        <NavLink to={SHOP_ROUTE}>
+                            <Image src={logo}/>
+                        </NavLink>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>*/}
+                        <div>
+                            <Button className="btn-secondary mx-5" onClick={toggleMenu}>
+                                Каталог
+                            </Button>
+                            {isOpen && (
+                                <div
+                                    className="burger-menu py-5 d-flex align-items-center border border-secondary"
+                                    style={{
+                                        position: "absolute",
+                                        left: 0,
+                                        top: "100%",
+                                        width: "100%",
+                                        backgroundColor: "white"
+                                    }}
+                                >
+                                    <Container>
+                                        <Row >
+                                            <Col className='d-flex flex-column justify-content-around align-items-start'>
+                                                <NavbarBrand className='text-dark fw-bold text-uppercase' style={{fontSize: 12}}>
+                                                    Одежда
+                                                </NavbarBrand>
+                                                <Nav className='d-flex flex-column mt-3 align-items-start'>
+                                                    <NavLink to={PRODUCT_ROUTE} style={{color: "grey"}}>Юбки</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Джемперы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Джинсы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Брюки, шорты</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Футболки, топы, лонгсливы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Блузки, рубашки, туники</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Платья</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Толстовки</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Костюмы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Оверол</NavLink>
+                                                </Nav>
+                                            </Col>
+                                            <Col className='d-flex flex-column justify-content-around align-items-start'>
+                                                <NavbarBrand className='text-dark fw-bold text-uppercase' style={{fontSize: 12}}>
+                                                    Верхняя одежда
+                                                </NavbarBrand>
+                                                <Nav className='d-flex flex-column mt-3 align-items-start'>
+                                                    <NavLink to={PRODUCT_ROUTE} style={{color: "grey"}}>Юбки</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Джемперы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Джинсы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Брюки, шорты</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Футболки, топы, лонгсливы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Блузки, рубашки, туники</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Платья</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Толстовки</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Костюмы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Оверол</NavLink>
+                                                </Nav>
+                                            </Col>
+                                            <Col className='d-flex flex-column justify-content-around align-items-start'>
+                                                <NavbarBrand className='text-dark fw-bold text-uppercase' style={{fontSize: 12}}>
+                                                    Аксессуары
+                                                </NavbarBrand>
+                                                <Nav className='d-flex flex-column mt-3 align-items-start'>
+                                                    <NavLink to={PRODUCT_ROUTE} style={{color: "grey"}}>Юбки</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Джемперы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Джинсы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Брюки, шорты</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Футболки, топы, лонгсливы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Блузки, рубашки, туники</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Платья</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Толстовки</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Костюмы</NavLink>
+                                                    <NavLink to='/' style={{color: "grey"}}>Оверол</NavLink>
+                                                </Nav>
+                                            </Col>
+                                            <Col className='d-flex flex-column justify-content-around align-items-start'>
+                                                <NavbarBrand className='text-dark fw-bold text-uppercase' style={{fontSize: 12}}>
+                                                    Жакеты и пиджаки
+                                                </NavbarBrand>
+                                                <NavbarBrand className='text-dark fw-bold text-uppercase' style={{fontSize: 12}}>
+                                                    Обувь
+                                                </NavbarBrand>
+                                                <NavbarBrand className='text-dark fw-bold text-uppercase' style={{fontSize: 12}}>
+                                                    Сумки и кошельки
+                                                </NavbarBrand>
+                                                <NavbarBrand className='text-dark fw-bold text-uppercase' style={{fontSize: 12}}>
+                                                    Шапки, шляпы, шарфы
+                                                </NavbarBrand>
+                                            </Col>
+                                        </Row>
+
+                                    </Container>
+                                </div>
+                            )}
+                        </div>
+
+                    </div>
+
+
 
                     {user.isAuth ?
                         <Nav className="ml-auto d-flex align-items-center" style={{color: 'white'}}>
