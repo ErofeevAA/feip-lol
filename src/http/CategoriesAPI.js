83a26394d5e44492e089e4becf512c20ba5a8556
Теминатор <erofeev.aa@students.dvfu.ru>
2023-06-22 01:52:59 +1000

    Update CategoriesAPI

--- src/http/CategoriesAPI.js
+++ src/http/CategoriesAPI.js
@@ -5,3 +5,16 @@ export const getCategories = async () => {
     const {data} = await $host.get('categories')
     return data
 }
+
+
+
+/// TODO Картинки крутим при создании??
+export const postParentCategory = async (name) => {
+    const {data} = await $authHost.post('/categories', {"name": name})
+    return data
+}
+
+export const postChildrenCategory = async (parentCategoryId, name) => {
+    const {data} = await $authHost.post(`/categories/${parentCategoryId}`, {"name": name})
+    return data
+}
